a332474a816daabb278600083cd0d082cf6da321
Isabel <iarcones@gmail.com>
2018-11-16 22:30:53 -0800

    version working, needs css changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,12 @@
 // VARIABLES
 
-var corrects = 0;
-var incorrect = 0;
-var unanswered = 0;
-
-var questionTimer;
-
+    var corrects = 0;
+    var incorrect = 0;
+    var unanswered = 0;
+    var timeCount = 15;
+    var index = 0;
+    var lastquestion = false;
+    var crono;
 
 var questions =[
     {question:"what's your name",
@@ -22,152 +23,138 @@ var questions =[
     correctAnswer:"Florida"
 }]
 
-console.log("questions.length: " + questions.length);
+$("#start").on("click", function () {
 
-// console.log("this is the answer for question 1 : "+ answers.questions[0])
-
-// FUNCTIONS
-
-// for (var i=0; i < questions.length; i++){
-//     console.log(questions[i])
-// for (var q=0; q < answers.question1.length; q++){
-//     console.log(answers.question1[q])
-// for (var a=0; a < rightAnswer.length; a++){
-//     console.log(rightAnswer[a])
-// }
-// }
-// }
-
-////////
-
-
-
-
-///// TIMER 
-
-
-
-
-// BODY
-
-
-
-// Insert the contents of the unsorted array into the #start div
-// document.querySelector("#start").innerText = unsortedArr.join(", ");
-
-// Get a reference to the go button in the DOM, add a click event listener to it
-
-
-var startBtn = document.querySelector("#start");
-
-startBtn.addEventListener("click", function() {
-    console.log("calling start");
-    start();
-
+    $("#start").attr({style: "visibility: hidden;"});
+    $(".areagame").attr({style: "visibility: visible;"});
+    $("#title").attr({style: "visibility: visible;"});
+    startTimer();
+    showQuestion();
+    
 });
 
-function questionsTrivia(){ 
-    console.log ("I am in questionsTrivia");
 
-    for (var i = 0; i < questions.length; i++) {
-        console.log ("I am in the for: " + i);
-        var time=30;
-     
-        while(time > 0 || correctQuestion === 0){
 
-            console.log ("I am in the while: " + time + " | " + correctQuestion);
-           
-            $("#question").text(questions[i].question);
-            $("#A1").text(questions[i].answers[0]);
-            $("#A2").text(questions[i].answers[1]);
-            $("#A3").text(questions[i].answers[2]);
-            $("#A4").text(questions[i].answers[3]);
 
-            var correctQuestion = 0;
+function showQuestion() {
 
-            intervalId = setInterval(count, 1000);
-           
-            var startBtn = document.querySelector(".answer");
-            startBtn.addEventListener("click", function() {
-                if (questions[i].answers[value] === questions[i].correctAnswer) {
-                    corrects++;
-                    correctQuestion = 1;
-                }
-                else {
-                    incorrect++;
-                    correctQuestion = 2;
-                }
-            });
-            
-        }
-        if (time === 0 || correctQuestion === 2) {
-            console.log("sorry the answer was: " + questions[i].correctAnswer);
+    $("#question").html("<h2>" + questions[index].question + "</h2>");
+   
+    $("#answers").empty();
+
+    for (j = 0; j < questions[index].answers.length; j++) {
+        optionBtn = $("<button>");
+        optionBtn.html(questions[index].answers[j]);
+        optionBtn.addClass("clickBtn");
+        optionBtn.val(j);
+        $("#answers").append(optionBtn);
+    }
+    
+    currentQuestion = questions[index];
+
+    index++;
+
+    $(".clickBtn").on("click", function () {
+    
+        stopTimer();
+
+        var answerValue = ($(this).attr("value"));
+
+        if (currentQuestion.answers[answerValue] === currentQuestion.correctAnswer) {
+            corrects++;
+            correctQuestion = 1;
+            $("#message").empty();
+            $("#message").html("<h2>Correct!!! Good Job :-), let's go for the next one</h2><br>");
+            results();
         }
         else {
-            console.log("great good answer: " + questions[i].correctAnswer);
-        } 
-        setTimeout(cleanAlert, 3000);
+            incorrect++;
+            correctQuestion = 2;
+            $("#message").empty();
+            $("#message").html("<h2>Nope!!! the correct answer was: " + currentQuestion.correctAnswer + " let's go for the next one</h2><br>");
+            results();
+        }
+    });
+};
+
+function startTimer() {
+
+    crono = setInterval(countDown, 1000);
+}
+
+function countDown() {
+
+    $("#timer").html("<p>Time Remaining: " + timeCount + " Seconds</p>");
+   
+    timeCount--;
+
+    if (timeCount < 0) {
+        stopTimer();
+        unanswered++;
+        $("#message").html("<p>Out of time. The correct answer is: " + currentQuestion.correctAnswer + "</p>");
+        results();
+ 
     }
+}
+
+function stopTimer() {
+    clearInterval(crono);
+
 }
 
 function results() {
    
-    $(".questions").attr({style: "visibility: hidden;"});
-    $(".results").attr({style: "visibility: visible;"});
-    $("#corrects").text(unanswered);
-    $("#incorrect").text(incorrect);
-    $("#unanswered").text(unanswered);
-    var startOverBtn = document.querySelector("#startOver");
-    startBtn.addEventListener("click", function() {
-       start();
+    setTimeout(function () {
+
+        $("#question").empty();
+        $("#answer").empty();
+
+        timeCount = 15;
+      
+        if (index === questions.length) {
+            gameOver();
+            
+        } else {
+            $("#message").html("<h3>Let's go for the next one :-)</h3>");
+            startTimer();
+            showQuestion();
+        }
+    }, 1500);
+
+
+}
+
+function gameOver(){
+    $("#question").empty();
+    $("#answers").empty();
+    $("#question").attr({style: "visibility: hidden;"});
+    $("#answers").attr({style: "visibility: hidden;"});
+    $("#message").empty();
+    $("#message").html("<h3>All done, here's how you did! </h3><br>");
+
+    $("#results").append("<h2> Correct Answers: " + corrects + "</h2><br>");
+    $("#results").append("<h2> Incorrect Answers: " + incorrect + "</h2><br>");
+    $("#results").append("<h2> Unanswered: " + unanswered + "</h2><br>");
+
+    $("#restart").attr({style: "visibility: visible;"});
+
+}
+
+$("#restart").on("click", function () {
+
+    $("#message").empty();
+    $("#question").attr({style: "visibility: visible;"});
+    $("#answers").attr({style: "visibility: visible;"});
+    corrects = 0;
+    incorrect = 0;
+    unanswered = 0;
+    timeCount = 15;
+    index = 0;
+    lastquestion = false;
+    crono;
     
-    });
-}
-
-function start() {
-
-    var time=30
-    $("#start").attr({style: "visibility: hidden;"});
-    $(".areagame").attr({style: "visibility: visible;"});
-    $(".questions").attr({style: "visibility: visible;"});
-    console.log("start");
-    console.log(questions[0].question);
-    questionsTrivia();
-
-}
-
-function count() {
-    time--;
-    $("#timing").text(time);
-  }
-
-function cleanAlert() {
-    /// clean the messages for previous question
-}
-
-// function timeRemaining() {
-// var clockRunning= false;
-// var time=30
-// var timmer = {
-
-    // reset: function() {
-    //   time = 30;
-    //   $("#display").text("00:00");
-    // },
+    $("#restart").attr({style: "visibility: hidden;"});
+    startTimer();
+    showQuestion();
     
-    // start: function() {
-    //   // DONE: Use setInterval to start the count here and set the clock to running.
-    //   if (!clockRunning) {
-    //     intervalId = setInterval(stopwatch.count, 1000);
-    //     clockRunning = true;
-       
-    //   }
-    // },
-    // stop: function() {
-    //   clearInterval(intervalId);
-    //   clockRunning = false;
-    // },
-
- 
- 
-  
+});
